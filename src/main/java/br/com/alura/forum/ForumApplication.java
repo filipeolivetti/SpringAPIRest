c974d26471b156119ba5780bb668db0c3996401e
filipe.olivetti <folivetti@hotmail.com>
2021-05-20 12:21:26 -0300

    Implementando a paginação.

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,11 +2,13 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class ForumApplication {
 
 	public static void main(String[] args) {
